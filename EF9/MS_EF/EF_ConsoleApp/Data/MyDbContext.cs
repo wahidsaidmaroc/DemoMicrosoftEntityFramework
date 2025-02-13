bc7576b8d5d5c0034ea9f1ee2b36ae12a41bb5be
SAID WAHID <said@teknologiate.com>
2025-02-13 11:51:44 +0100

    Creation Projet de Base

---
+++ EF9/MS_EF/EF_ConsoleApp/Data/MyDbContext.cs
@@ -0,0 +1,41 @@
+﻿using EF_ConsoleApp.Entites;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+
+namespace EF_ConsoleApp.Data;
+
+public class MyDbContext : DbContext
+{
+    //Create Construcrtor
+    //public MyDbContext(DbContextOptions<MyDbContext> options) : base(options)
+    //{
+    //}
+
+
+    #region "DbSet"
+    public DbSet<Produit> Produits { get; set; }
+    #endregion
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        var configuration = new ConfigurationBuilder()
+               .AddJsonFile("appsettings.json")
+                .Build();
+
+        var constr = configuration.GetSection("ConnectionStrings:ZoneToLearnDb").Value;
+
+
+
+        optionsBuilder.UseSqlServer(constr);
+    }
+
+    //Create OnModelCreating method
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Produit>();
+    }
+
+
+
+}
